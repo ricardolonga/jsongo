4a51b109f75f24ac8a14907fcce23bb765283ccc
Ricardo Longa <ricardo.longa@gmail.com>
2015-08-09 16:41:09 -0300

    Novos testes.

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Fluent API to make it easier to create Json objects.
 
 [![Build Status](https://travis-ci.org/ricardolonga/jsongo.svg)](https://travis-ci.org/ricardolonga/jsongo)
 [![](http://gocover.io/_badge/github.com/ricardolonga/jsongo)](http://gocover.io/github.com/ricardolonga/jsongo)
-[![Coverage Status](https://coveralls.io/repos/ricardolonga/jsongo/badge.svg?branch=master&service=github)](https://coveralls.io/github/ricardolonga/jsongo?branch=master)
--- jsongo_test.go
+++ jsongo_test.go
@@ -15,8 +15,16 @@ func Test_create_empty_object(t *testing.T) {
 }
 
 func Test_create_populated_object(t *testing.T) {
-	expect := bytes2json([]byte(`{"name":"Ricardo Longa","idade":28,"skills":["Golang","Android"]}`))
-	result := Object().Put("name", "Ricardo Longa").Put("idade", 28).Put("skills", Array().Put("Golang").Put("Android"))
+	expect := bytes2json([]byte(`{"name":"Ricardo Longa","idade":28,"owner":true,"skills":["Golang","Android"]}`))
+	result := Object().Put("name", "Ricardo Longa").Put("idade", 28).Put("owner", true).Put("skills", Array().Put("Golang").Put("Android"))
+
+	check(t, struct2json(expect), struct2json(result))
+}
+
+func Test_create_two_populated_objects_into_array(t *testing.T) {
+	expect := bytes2json([]byte(`{"funcionarios":[{"name":"Ricardo Longa","idade":28,"skills":["Golang","Android"]},{"name":"Hery Victor","idade":32,"skills":["Golang","Java"]}]}`))
+	result := Object().Put("funcionarios", Array().Put(Object().Put("name", "Ricardo Longa").Put("idade", 28).Put("skills", Array().Put("Golang").Put("Android"))).Put(Object().
+																Put("name", "Hery Victor").Put("idade", 32).Put("skills", Array().Put("Golang").Put("Java"))))
 
 	check(t, struct2json(expect), struct2json(result))
 }
@@ -27,6 +35,7 @@ func bytes2json(data []byte) map[string]interface{} {
 	err := json.Unmarshal(data, &jsonData)
 
 	if err != nil {
+		log.Printf("Erro: %s", err)
 		return nil
 	}
 
@@ -35,9 +44,9 @@ func bytes2json(data []byte) map[string]interface{} {
 
 func struct2json(data interface{}) string {
 	byteArray, err := json.Marshal(data)
-	//	byteArray, err = json.MarshalIndent(pessoa, "", "    ")
 
 	if err != nil {
+		log.Printf("Erro: %s", err)
 		return ""
 	}
 
@@ -48,6 +57,4 @@ func check(t *testing.T, expect, result interface{}) {
 	if !reflect.DeepEqual(expect, result) {
 		t.Errorf("\n\nExpect: %s\nResult: %s", expect, result)
 	}
-
-	log.Printf("")
 }
